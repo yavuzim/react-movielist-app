362f24711667e217ee8f8c51aabe9d4365390fa5
90539 <yavuz.imer133@gmail.com>
2021-08-18 10:12:55 +0300

    filmlerin arraydan çekilmesi ve silme işlemi yapıldı

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,42 @@ class App extends React.Component {
         movies: [
             {
                 "id": 1,
-                "name": "The Flash",
+                "name": "The Batman",
                 "rating": 8.3,
                 "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://l24.im/5KM4WOd"
+                "imgURL": "https://cdn.kayiprihtim.com/wp-content/uploads/2018/11/batman-begins-kapak.jpg"
             },
             {
                 "id": 2,
                 "name": "Interstellar",
                 "rating": 6.8,
                 "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://l24.im/C3JS"
+                "imgURL": "https://tr.web.img2.acsta.net/pictures/14/10/09/15/52/150664.jpg"
             },
             {
                 "id": 3,
                 "name": "Arrow",
                 "rating": 7.9,
                 "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://l24.im/hVwuf2"
+                "imgURL": "https://dizitanitim.files.wordpress.com/2014/12/wpid-wp-1417891985968.jpeg"
+            },
+            {
+                "id": 4,
+                "name": "PK",
+                "rating": 7.9,
+                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
+                "imgURL": "https://www.intersinema.com/film/resimler/11/11655_a_1100.jpg"
             }
         ]
+    }
+
+    deleteMovie = (movie) => {
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        )
+        this.setState({
+            movies: newMovieList
+        })
     }
 
     render() {
@@ -37,7 +53,10 @@ class App extends React.Component {
                         <SearchBar />
                     </div>
                 </div>
-                <MovieList />
+                <MovieList
+                    movies={this.state.movies}
+                    deleteMovieProp={this.deleteMovie}
+                />
             </div>
         )
     }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,57 +1,27 @@
 import React from 'react';
 
-class MovieList extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-lg-4">
+const MovieList = (props) => {
+    return (
+        <div className="row">
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
-                        <img src="https://tr.web.img2.acsta.net/pictures/14/10/09/15/52/150664.jpg" className="card-img-top" alt="Sample Movie" />
+                        <img src={movie.imgURL} className="card-img-top" alt="Sample Movie" />
                         <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
+                            <h5 className="card-title">{movie.name}</h5>
+                            <p className="card-text">{movie.overview}</p>
                             <div className="d-flex justify-content-between align-item-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                <button type="button" className="btn btn-md btn-outline-danger" onClick={(event)=>props.deleteMovieProp(movie)}>Delete</button>
                                 <h2>
-                                    <span className="badge badge-info">9.0</span>
+                                    <span className="badge badge-primary" style={{ background: 'blue' }}>{movie.rating}</span>
                                 </h2>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src="https://tr.web.img2.acsta.net/r_1280_720/medias/nmedia/18/35/23/40/18431141.jpg" className="card-img-top" alt="Sample Movie" />
-                        <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
-                            <div className="d-flex justify-content-between align-item-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2>
-                                    <span className="badge badge-info">9.0</span>
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src="https://tr.web.img4.acsta.net/medias/nmedia/18/85/31/56/20125440.jpg" className="card-img-top" alt="Sample Movie" />
-                        <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
-                            <div className="d-flex justify-content-between align-item-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2>
-                                    <span className="badge badge-info">9.0</span>
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+            ))}
+        </div>
+    )
 }
 
-export default MovieList 
+export default MovieList
