29300281f9e11ea6ac3c70ab6bb30c796d79457c
90539 <yavuz.imer133@gmail.com>
2021-08-18 17:52:11 +0300

    json dosyasından veriler çekildi

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,49 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
-
+import axios from 'axios';
 class App extends React.Component {
     state = {
-        movies: [
-            {
-                "id": 1,
-                "name": "The Batman",
-                "rating": 8.3,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://cdn.kayiprihtim.com/wp-content/uploads/2018/11/batman-begins-kapak.jpg"
-            },
-            {
-                "id": 2,
-                "name": "Interstellar",
-                "rating": 6.8,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://tr.web.img2.acsta.net/pictures/14/10/09/15/52/150664.jpg"
-            },
-            {
-                "id": 3,
-                "name": "Arrow",
-                "rating": 7.9,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://dizitanitim.files.wordpress.com/2014/12/wpid-wp-1417891985968.jpeg"
-            },
-            {
-                "id": 4,
-                "name": "PK",
-                "rating": 7.9,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://www.intersinema.com/film/resimler/11/11655_a_1100.jpg"
-            }, {
-                "id": 5,
-                "name": "Kolpaçino",
-                "rating": 6.8,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "https://upload.wikimedia.org/wikipedia/tr/9/9f/Kolpa%C3%A7ino.jpg"
-            }, {
-                "id": 6,
-                "name": "Recep İvedik 4 ",
-                "rating": 6.8,
-                "overview": "Some quick example text to build on the card title and make up the bulk of the card's content. ",
-                "imgURL": "http://i.imgur.com/T9AMP4o.jpg"
-            }
-        ],
+        movies: [],
         searchQuery: ""
     }
 
-    deleteMovie = (movie) => {
+    // FETCH API İLE VERİ ÇEKME
+    /*   async componentDidMount() {
+          const baseURL = "http://localhost:3002/movies"
+          const response = await fetch(baseURL)
+          const data = await response.json();
+          this.setState({ movies: data })
+      } */
+
+    // AXIOS API İLE VERİ ÇEKME
+    async componentDidMount() {
+        const response = await axios.get("http://localhost:3002/movies")
+        this.setState({ movies: response.data })
+    }
+
+    // FETCH API İLE VERİ SİLME
+    /* deleteMovie = async (movie) => {
+
+        const baseURL = `http://localhost:3002/movies/${movie.id}`
+        await fetch(baseURL, {
+            method: "DELETE"
+        })
+
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        )
+
+        this.setState(state => ({
+            movies: newMovieList
+        }))
+    } */
+
+    // AXIOS API İLE VERİ SİLME
+    deleteMovie = async (movie) => {
+
+        axios.delete(`http://localhost:3002/movies/${movie.id}`)
+
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         )
